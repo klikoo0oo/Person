e34d159ca70a35e4ad83806f59b8e2e3f3eb1677
danii <danii@DESKTOP-KG3VLA1>
2023-02-07 16:50:46 +0300

    Добавьте файлы проекта.

---
+++ Person1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Person
+{
+    string FullName;
+    byte age;
+
+    public void move()
+    {
+        Console.WriteLine($"Cейчас говорит {FullName}");
+    }
+
+    public void talk()
+    {
+        Console.WriteLine($"Cейчас говорит {FullName}");
+    }
+
+    public Person() { }
+
+    public Person(string FullName, byte age)
+    {
+        this.FullName = FullName;
+        this.age = age;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Вася", 17);
+        Person person1 = new Person();
+
+        person1.talk();
+        person.move();
+
+    }
+
+}
